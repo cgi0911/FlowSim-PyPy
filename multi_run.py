6cfb2e02dc026b8158fdcc2bcb5ec26e3329e054
Kuan-yin Chen <cgi0911@gmail.com>
2015-05-04 15:51:37 -0400

    minor update

--- multi_run.py
+++ multi_run.py
@@ -11,20 +11,17 @@ __copyright__   = 'Copyright 2014, NYU-Poly'
 import os
 import multiprocessing as mp
 
-N_WORKERS = 2
+N_WORKERS = 3
 INTERPRETER = '~/Links/pypy'
 # TASKS = ['./cfgs/ecmp.txt', './cfgs/spf.txt', './cfgs/fe2.txt',\
 #          './cfgs/fe3.txt', './cfgs/fe4.txt', './cfgs/fe5.txt',\
 #          './cfgs/fe6.txt', './cfgs/fe7.txt', './cfgs/fe8.txt',\
 #          './cfgs/fe9.txt', './cfgs/fe10.txt']
-TASKS = ['./cfgs/all_rd_nr/all_rd_nr_650fps_unlmt', './cfgs/all_rd_nr/all_rd_nr_700fps_unlmt', './cfgs/all_rd_nr/all_rd_nr_750fps_unlmt', './cfgs/all_rd_nr/all_rd_nr_800fps_unlmt', 
-         './cfgs/all_rd_nr/all_rd_nr_850fps_unlmt', './cfgs/all_rd_nr/all_rd_nr_900fps_unlmt', './cfgs/all_rd_nr/all_rd_nr_950fps_unlmt', './cfgs/all_rd_nr/all_rd_nr_1000fps_unlmt',
-         './cfgs/all_ec_nr/all_ec_nr_650fps_unlmt', './cfgs/all_ec_nr/all_ec_nr_700fps_unlmt', './cfgs/all_ec_nr/all_ec_nr_750fps_unlmt', './cfgs/all_ec_nr/all_ec_nr_800fps_unlmt', 
-         './cfgs/all_ec_nr/all_ec_nr_850fps_unlmt', './cfgs/all_ec_nr/all_ec_nr_900fps_unlmt', './cfgs/all_ec_nr/all_ec_nr_950fps_unlmt', './cfgs/all_ec_nr/all_ec_nr_1000fps_unlmt',
-         './cfgs/all_fe_nr/all_fe_nr_650fps_unlmt', './cfgs/all_fe_nr/all_fe_nr_700fps_unlmt', './cfgs/all_fe_nr/all_fe_nr_750fps_unlmt', './cfgs/all_fe_nr/all_fe_nr_800fps_unlmt', 
-         './cfgs/all_fe_nr/all_fe_nr_850fps_unlmt', './cfgs/all_fe_nr/all_fe_nr_900fps_unlmt', './cfgs/all_fe_nr/all_fe_nr_950fps_unlmt', './cfgs/all_fe_nr/all_fe_nr_1000fps_unlmt',
-         './cfgs/one_spf_nr/one_spf_nr_650fps_unlmt', './cfgs/one_spf_nr/one_spf_nr_700fps_unlmt', './cfgs/one_spf_nr/one_spf_nr_750fps_unlmt', './cfgs/one_spf_nr/one_spf_nr_800fps_unlmt', 
-         './cfgs/one_spf_nr/one_spf_nr_850fps_unlmt', './cfgs/one_spf_nr/one_spf_nr_900fps_unlmt', './cfgs/one_spf_nr/one_spf_nr_950fps_unlmt', './cfgs/one_spf_nr/one_spf_nr_1000fps_unlmt',]
+TASKS = ['./cfgs/all_fe_nr/all_fe_nr_650fps_unlmt.txt', './cfgs/all_fe_nr/all_fe_nr_700fps_unlmt.txt', './cfgs/all_fe_nr/all_fe_nr_750fps_unlmt.txt', './cfgs/all_fe_nr/all_fe_nr_800fps_unlmt.txt', 
+         './cfgs/all_fe_nr/all_fe_nr_850fps_unlmt.txt', './cfgs/all_fe_nr/all_fe_nr_900fps_unlmt.txt', './cfgs/all_fe_nr/all_fe_nr_950fps_unlmt.txt', './cfgs/all_fe_nr/all_fe_nr_1000fps_unlmt.txt',
+         './cfgs/one_spf_nr/one_spf_nr_650fps_unlmt.txt', './cfgs/one_spf_nr/one_spf_nr_700fps_unlmt.txt', './cfgs/one_spf_nr/one_spf_nr_750fps_unlmt.txt', './cfgs/one_spf_nr/one_spf_nr_800fps_unlmt.txt', 
+         './cfgs/one_spf_nr/one_spf_nr_850fps_unlmt.txt', './cfgs/one_spf_nr/one_spf_nr_900fps_unlmt.txt', './cfgs/one_spf_nr/one_spf_nr_950fps_unlmt.txt', './cfgs/one_spf_nr/one_spf_nr_1000fps_unlmt.txt']
+
 NOHUP_PATH = './nohups'
 
 def do_work(task_queue):
